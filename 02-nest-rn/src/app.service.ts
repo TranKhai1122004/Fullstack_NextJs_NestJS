11fcae503b60265424c7585903c349c922f9ba44
TranKhai1122004 <trankhai112204@gmail.com>
2025-10-05 17:40:04 +0700

    update base models

--- 02-nest-rn/src/app.service.ts
+++ 02-nest-rn/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Trần Khải Hello World!';
+    return 'Hello World!';
   }
 }
