62fcec21a4436d5b040f7d10367b85c979de3c24
TranKhai1122004 <trankhai112204@gmail.com>
2025-11-05 22:16:06 +0700

    update valid username/password

--- 02-nest-rn/src/modules/users/dto/update-user.dto.ts
+++ 02-nest-rn/src/modules/users/dto/update-user.dto.ts
@@ -1,4 +1,20 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsMongoId, IsNotEmpty, IsOptional } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+
+export class UpdateUserDto {
+    @IsMongoId({ message: "Invalid ID" })
+    @IsNotEmpty({ message: "ID cannot be empty." })
+    _id: string;
+
+    @IsOptional()
+    name: string
+
+    @IsOptional()
+    phone: string;
+
+    @IsOptional()
+    address: string;
+
+    @IsOptional()
+    image: string;
+}
