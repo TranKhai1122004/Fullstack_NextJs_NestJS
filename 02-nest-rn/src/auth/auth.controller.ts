550ec4e045d786e8fda39ec40e80636471a60eb3
TranKhai1122004 <trankhai112204@gmail.com>
2025-11-29 23:17:17 +0700

    verify email

--- 02-nest-rn/src/auth/auth.controller.ts
+++ 02-nest-rn/src/auth/auth.controller.ts
@@ -4,12 +4,16 @@ import { LocalAuthGuard } from './passport/local-auth.guard';
 import { JwtAuthGuard } from './passport/jwt-auth.guard';
 import { Public } from '@/decorator/customize';
 import { CreateAuthDto } from './dto/create-auth.dto';
-
+import { MailerService } from '@nestjs-modules/mailer';
 
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly mailerService: MailerService,
+  ) { }
 
   @Post("login")
   @Public()
@@ -23,6 +27,22 @@ export class AuthController {
   @Public()
   register(@Body() registerDto: CreateAuthDto) {
     return this.authService.handleRegister(registerDto);
+  }
+
+  @Get('mail')
+  @Public()
+  testMail() {
+    this.mailerService
+      .sendMail({
+        to: 'trankhai1122004@gmail.com', // list of receivers
+        subject: 'Testing Nest MailerModule ✔', // Subject line
+        text: 'welcome', // plaintext body
+        html: '<b>Hello world with TrKhai</b>', // HTML body content
+      })
+      .then(() => { })
+      .catch(() => { });
+
+    return 'ok';
   }
 
 
